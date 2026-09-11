6c0f4bff0c33a77bc1d7ad82dbd4f8c5db7c56a5
SuryaHuang <suryahuang1996@gmail.com>
2026-09-11 08:12:17 +0800

    membuat components: navbar

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -3,10 +3,10 @@ import { ShowNotification } from "@/features/auth/components/ShowNotification";
 
 const SignInPage = () => {
   return (
-    <main>
-      <div>
+    <main className="min-h-screen p-8">
+      <div className="mt-30">
         <ShowNotification />
-        <h1 className="flex flex-col gap-4 w-full mt-30 text-center font-bold text-3xl">
+        <h1 className="flex flex-col gap-4 w-full text-center font-bold text-3xl">
           Sign In
         </h1>
         <SignInForm />
